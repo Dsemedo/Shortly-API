f0d0e086c404c5994d6ca5fc493d94b896974056
Douglas Semedo <dsemedoo@gmail.com>
2022-12-22 17:36:58 -0300

    Feat: Rankin and Users routes - Done

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 import signupRouter from "./routes/signupRouter.js";
 import signinRouter from "./routes/signinRouter.js";
 import urlRouter from "./routes/urlRouter.js";
+import userRouter from "./routes/usersRouter.js";
+import rankingRouter from "./routes/rankingRouter.js";
 
 const app = express();
 app.use(cors());
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(urlRouter);
+app.use(userRouter);
+app.use(rankingRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on ${port}`));
